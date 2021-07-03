37cd6013720d63952ad01f6a9a979b5082d9947d
SangeethKrishna2006 <81748900+SangeethKrishna2006@users.noreply.github.com>
2021-07-03 18:16:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,17 +12,17 @@ var cars, car1, car2, car3, car4;
 
 var track, car1_img, car2_img, car3_img, car4_img;
 
-function preload(){
-  track = loadImage("../images/track.jpg");
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
+function preload() {
+  track = loadImage("images/track.jpg");
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
 }
 
-function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+function setup() {
+  canvas = createCanvas(displayWidth - 20, displayHeight - 30);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -30,15 +30,15 @@ function setup(){
 }
 
 
-function draw(){
-  if(playerCount === 4){
+function draw() {
+  if (playerCount === 4) {
     game.update(1);
   }
-  if(gameState === 1){
+  if (gameState === 1) {
     clear();
     game.play();
   }
-  if(gameState === 2){
+  if (gameState === 2) {
     game.end();
   }
 }
